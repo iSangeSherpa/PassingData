d7a13933e712855e15eb480991392f8413c23d1c
Sange Sherpa <sherpasange1215@gmail.com>
2023-05-10 14:03:23 +0545

    Initial Commit

---
+++ PassingData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassingData
+//
+//  Created by Sange Sherpa on 10/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
